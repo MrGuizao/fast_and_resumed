392b28fdb3601c66b0c08b767061654fc4ae945c
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-17 10:58:04 -0300

    exercicio TODO dia 17/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+  const [isClicked, setIsClicked] = useState(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTodos([...todos, { id: crypto.randomUUID(), task }])
+    setTask('')
+  }
+
+  const handleChange = e => setTask(e.target.value);
+  const handleClick = id => setIsClicked(id);
+  const deleteTask = id => setTodos(todos.filter(todo => todo.id !== id));
+
   return (
-    <main>
-    
-    </main>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task} onChange={handleChange} />
+        <button>add</button>
+      </form>
+      <ul>
+        {
+          todos.map(todo => {
+            return (
+              <div
+                key={todo.id}
+                style={isClicked === todo.id ? { color: 'red' } : null}>
+                <li onClick={() => handleClick(todo.id)}
+                >{todo.task}</li>
+                <span onClick={() => deleteTask(todo.id)}>X</span>
+              </div>
+            )
+          }
+          )
+        }
+      </ul>
+    </>
   )
 }
 
